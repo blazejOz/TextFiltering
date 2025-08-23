c10448a9a8b37d8f41bbc5a805acb09503e109b5
blazejOz <blazej.ozog@gmail.com>
2025-08-23 12:29:37 +0200

    resolve

--- src/UppercaseFilter.h
+++ src/UppercaseFilter.h
@@ -1,10 +1,9 @@
 #pragma once
 #include "BaseFilter.h"
-#include <vector>
 
 class UppercaseFilter : public BaseFilter
 {
 public:
-    virtual std::string apply(const std::string& in) override;
+    const std::string apply(const std::string& in) override;
 
 };
