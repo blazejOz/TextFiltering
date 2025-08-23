5fb35a75d1fb6e8b9c16a620d03a697d5e98c0d9
blazejOz <blazej.ozog@gmail.com>
2025-08-23 11:47:24 +0200

    first draft upper case filter

--- src/UppercaseFilter.cpp
+++ src/UppercaseFilter.cpp
@@ -5,6 +5,10 @@
 std::string UppercaseFilter::apply(const std::string& in) 
 {
     std::string out = in;
+<<<<<<< HEAD
+=======
+    std::string out = in;
+>>>>>>> 94bdc61 (first draft upper case filter)
     std::transform(out.begin(), out.end(), out.begin(),
                    [](unsigned char c){ return std::toupper(c); });
     return out;
