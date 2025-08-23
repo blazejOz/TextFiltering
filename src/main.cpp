efa9a98399ef784b24ae6636fb78a5e77285df62
blazejOz <blazej.ozog@gmail.com>
2025-08-23 12:06:05 +0200

    fixed orignial not showing and added UppercaseFilter

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,23 @@
 #include <iostream>
 #include "BaseFilter.h"
 #include "CensorFilter.h"
+#include "UppercaseFilter.h"
 
 int main(int argc, char** argv) {
     std::vector<BaseFilter*> filters;
     filters.push_back(new CensorFilter({
                 "walk"
                 }));
+    filters.push_back(new UppercaseFilter());
     for (int i = 1; i<argc; i++)
     {
-        std::string transformed = std::string(argv[i]);
-        std::string original = transformed;
+        std::string original = std::string(argv[i]);
+        std::string transformed = original;
         for (auto* f : filters)
         {
             transformed = f->apply(transformed);
         }
-        std::cout<<"Original input: "<<transformed<<" Transformed to: " <<transformed<<std::endl;
+        std::cout<<"Original input: \""<<original<<"\" Transformed to: \"" <<transformed<<"\"" <<std::endl;
     }
     return 0;
 }
