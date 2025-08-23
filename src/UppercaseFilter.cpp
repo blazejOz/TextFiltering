2afa233aaf00570ae0f22633691b72be8c8ddcf7
blazejOz <blazej.ozog@gmail.com>
2025-08-23 11:57:14 +0200

    Tests work, upper case works

--- src/UppercaseFilter.cpp
+++ src/UppercaseFilter.cpp
@@ -5,7 +5,6 @@
 std::string UppercaseFilter::apply(const std::string& in) 
 {
     std::string out = in;
-    std::string out = in;
     std::transform(out.begin(), out.end(), out.begin(),
                    [](unsigned char c){ return std::toupper(c); });
     return out;
