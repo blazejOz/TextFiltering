72f32567337e62f140d5c33eace963f4d731f881
blazejOz <blazej.ozog@gmail.com>
2025-08-24 06:47:16 +0200

    .

--- src/UppercaseFilter.cpp
+++ src/UppercaseFilter.cpp
@@ -5,13 +5,6 @@
 std::string UppercaseFilter::apply(const std::string& in) 
 {
     std::string out = in;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    std::string out = in;
->>>>>>> 94bdc61 (first draft upper case filter)
-=======
->>>>>>> 2afa233 (Tests work, upper case works)
     std::transform(out.begin(), out.end(), out.begin(),
                    [](unsigned char c){ return std::toupper(c); });
     return out;
